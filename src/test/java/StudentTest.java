247b317265e16b87dfa0e400e0a5d859f220461d
NoctisSolvane <fhdarklord173@gmail.com>
2026-03-03 22:24:05 +0530

    Added another method getAvgMarks() for calculating avg marks

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 public class StudentTest{
 
@@ -22,15 +23,24 @@ public class StudentTest{
     }
 
     @Test
-    void testIsPassed_True() {
+    void testisPassed_True() {
         Student s = new Student("Test", 1, 50.0);
-        assertTrue(s.IsPassed());
+        assertTrue(s.isPassed());
     }
 
     @Test
-    void testIsPassed_False() {
+    void testisPassed_False() {
         Student s = new Student("Test", 1, 39.0);
-        assertFalse(s.IsPassed());
+        assertFalse(s.isPassed());
+    }
+
+    @Test
+    void testGetAvgMarks() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Test1", 1, 45.0));
+        students.add(new Student("Test2", 1, 98.0));
+        students.add(new Student("Test3", 1, 83.0));
+        assertEquals(( 45.0 + 98.0 + 83.0 ) / 3, Student.getAvgMarks(students), 0.001);
     }
 }
 
